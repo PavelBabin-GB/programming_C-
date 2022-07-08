bf6707b0f8f279f8721348e9456a2d9243e9dc7a
pavel <pavel-babin@yandex.ru>
2022-07-08 16:11:08 +0500

    Ex 003 Sum 1

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numbeA = 31;
-int numbeB = 5;
-int result = numbeA + numbeB;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
