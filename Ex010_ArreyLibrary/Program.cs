998286cb50e2337658f40ee66340c92cb213dc71
pavel <pavel-babin@yandex.ru>
2022-07-12 19:06:45 +0500

    Ex 010 ArreyLibrary

---
+++ Ex010_ArreyLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿//Заполняет массив
+void FillArrey(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 20);
+        index++;
+    }
+}
+//Печатает массив
+void PrintArrey(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+// Находит индекс элемента в массиве
+int IndexOF(int[] collection, int find)
+{
+    int Length = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < Length)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+//Определяет из скольки элементов создать массив
+int[] arrey = new int[10];
+FillArrey(arrey);
+PrintArrey(arrey);
+Console.WriteLine();
+int pos = IndexOF(arrey, 5);
+Console.WriteLine(pos);
