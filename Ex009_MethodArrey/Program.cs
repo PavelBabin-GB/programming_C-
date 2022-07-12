fb50d1ae2d47180a26b3948774c40adb3e42a590
pavel <pavel-babin@yandex.ru>
2022-07-12 17:23:49 +0500

    Ex 009 MethoddArrey

---
+++ Ex009_MethodArrey/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arrey = {22, 48, 12, 52, 67, 12, 69, 23, 78};
+int n = arrey.Length;
+int find = 12;
+int index = 0;
+
+while (index < n)
+{
+    if (arrey[index] == find )
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
