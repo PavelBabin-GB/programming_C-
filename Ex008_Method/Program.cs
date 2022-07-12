ce8743b3aa4cf362288514a7454a8c9e23c71a93
pavel <pavel-babin@yandex.ru>
2022-07-12 16:57:07 +0500

    Ex 008 Arrey

---
+++ Ex008_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+int[] arrey = {12,20,36,14,5,55,72,18,91};
+
+
+int max = Max(Max(arrey[0], arrey[1], arrey[2]), 
+              Max(arrey[3], arrey[4], arrey[5]),
+              Max(arrey[6], arrey[7], arrey[8]));
+Console.WriteLine(max);
