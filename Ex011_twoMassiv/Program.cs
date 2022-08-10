5a406eae2a30a3fc7086f756a43e05f222ece620
pavel <pavel-babin@yandex.ru>
2022-08-10 15:05:25 +0500

    Program.cs

---
+++ Ex011_twoMassiv/Program.cs
@@ -0,0 +1,28 @@
+﻿// 
+void PrintArrey(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j] }");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArry(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+        //Console.WriteLine();
+    }
+}
+int[,] matrix = new int[3, 6];
+PrintArrey(matrix);
+FillArry(matrix);
+Console.WriteLine();
+PrintArrey(matrix);
